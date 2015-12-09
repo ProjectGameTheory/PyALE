75ce98cc54cdf4bf114c16ce3c50dc599df466d4
Mathieu Reymond <mathieu.reymond@gmail.com>
2015-12-10 00:14:52 +0100

    Binary state representation, strips state converter

--- src/environments/RoomWorld.py
+++ src/environments/RoomWorld.py
@@ -2,95 +2,132 @@ from MultiEnvironment import MultiEnvironment
 import numpy as np
 import random
 
+
 class RoomWorld(MultiEnvironment):
-	def __init__(self, grid, population_size=0, ids=None, begins=None, goal=[0,0], *args, **kwargs):
-		self.grid = np.array(grid)
-		self.size = np.shape(self.grid)
-		super(RoomWorld, self).__init__(*args, **kwargs)
-		# Number of agents in this environment
-		self.population_size = population_size
-		# Begin positions of agents
-		self.begins = dict(zip(ids,begins)) if len(ids) == len(begins) else {}
-		# Goal position of all agents
-		self.goal = goal
-		if goal is None:
-			self.goal = self.random_position()
-		# 4 possible moves
-		self.directions = np.array([[1,0], [0,1], [-1,0], [0, -1]])
-		# Flags to collect
-		self.flags_taken = dict()
-		for agent_id in ids:
-			self.flags_taken[agent_id] = []
-		return
 
-	def random_position(self):
-		return np.array([random.randint(0, self.size[0]), random.randint(0, self.size[1])])
+    def __init__(self, grid, population_size=0, ids=None, begins=None, goal=[0, 0], *args, **kwargs):
+        self.grid = grid
+        self.size = np.shape(grid)
+        # register the names of all flags
+        self.flags = []
+        for row in self.grid:
+            for pos in row:
+                if pos[1]:
+                    self.flags.append(pos[1])
+        super(RoomWorld, self).__init__(*args, **kwargs)
+        # Number of agents in this environment
+        self.population_size = population_size
+        # Begin positions of agents
+        self.current_states = dict(zip(ids, begins)) if len(
+            ids) == len(begins) else {}
+        # Goal position of all agents
+        self.goal = goal
+        if goal is None:
+            self.goal = self.random_position()
+        # 4 possible moves
+        self.directions = np.array([[1, 0], [0, 1], [-1, 0], [0, -1]])
+        # Flags to collect
+        self.flags_taken = dict()
+        for agent_id in ids:
+            self.flags_taken[agent_id] = []
 
-	def get_cell(self,position):
-		return self.grid[position[0]][position[1]]
+    def random_position(self):
+        return np.array([random.randint(0, self.size[0]), random.randint(0, self.size[1])])
 
-	def vertical_wall(self, position):
-		walls = self.get_cell(position)[0]
-    		return int(walls) & 1
+    def get_cell(self, position):
+        return self.grid[position[0]][position[1]]
 
-	def horizontal_wall(self, position):
-		walls = self.get_cell(position)[0]
-    		return int(walls) & 2
+    def vertical_wall(self, position):
+        walls = self.get_cell(position)[0]
+        return int(walls) & 1
 
-	def move(self, position, direction):
-		#Direction 0=up, 1=right, 2=down, 3=left
-		new_position = position + self.directions[direction]
-		if direction == 0 and (new_position[0] >= self.size[0] or self.horizontal_wall(new_position)):
-			return position
-		elif direction == 2 and (new_position[0] < 0 or self.horizontal_wall(position)):
-			return position
-		elif direction == 1 and (new_position[1] >= self.size[1] or self.vertical_wall(new_position)):
-			return position
-		elif direction == 3 and (new_position[1] < 0 or self.vertical_wall(new_position)):
-			return position
-		else:
-			return new_position
+    def horizontal_wall(self, position):
+        walls = self.get_cell(position)[0]
+        return int(walls) & 2
 
-	def terminal(self, position):
-		print self.goal
-		print position
-		return np.array_equal(position, self.goal)
+    def move(self, position, direction):
+        # Direction 0=up, 1=right, 2=down, 3=left
+        new_position = position + self.directions[direction]
+        if direction == 0 and (new_position[0] >= self.size[0] or self.horizontal_wall(new_position)):
+            return position
+        elif direction == 2 and (new_position[0] < 0 or self.horizontal_wall(position)):
+            return position
+        elif direction == 1 and (new_position[1] >= self.size[1] or self.vertical_wall(new_position)):
+            return position
+        elif direction == 3 and (new_position[1] < 0 or self.vertical_wall(new_position)):
+            return position
+        else:
+            return new_position
 
-	def at_flag(self, position):
-		return self.get_cell(position)[1]
+    def terminal(self, position):
+        return np.array_equal(position, self.goal)
 
-	def flag_taken(self, position):
-		flag = self.get_cell(position)[1]
-		return (flag and any(flag in agent_flags for agent_flags in self.flags_taken.values()))
+    def at_flag(self, position):
+        return self.get_cell(position)[1]
 
-	def take_flag(self, id, position):
-		flag = self.get_cell(position)[1]
-		if flag:
-			self.flags_taken[id].append(flag)
+    def flag_taken(self, position):
+        flag = self.get_cell(position)[1]
+        return (flag and any(flag in agent_flags for agent_flags in self.flags_taken.values()))
 
-	def reward(self, id, position):
-		reward = 0
-		if self.terminal(position):
-			flags_collected = len(self.flags_taken[id])
-			reward = 100 * flags_collected
-		return reward
+    def take_flag(self, id, position):
+        flag = self.get_cell(position)[1]
+        if flag:
+            self.flags_taken[id].append(flag)
 
-	def to_binary(self, position):
-		state = np.zeros(self.size[0]*self.size[1])
-		state[position[0]+position[1]*self.size[0]] = 1
-		return state
+    def reward(self, id, position):
+        reward = 0
+        if self.terminal(position):
+            flags_collected = len(self.flags_taken[id])
+            reward = 100 * flags_collected
+        return reward
 
-	def start(self, id):
-		self.current_states[id] = self.begins[id]
-		return self.to_binary(self.begins[id])
+    # state of an agent:
+    # 1: his position encoded in binary
+    # 2: others position encoded in binary
+    # 3: flags left in room (for each flag: 0 for taken, 1 for available)
+    # 4: flags the agent has (for each flag: 1 for taken, 0 otherwise)
+    # agent needs to know both the flags he has and the flags left on board
+    # as others could also possess flags
+    # (but it doesn't matter to the agent which other agent has a specific flag
+    # , it's still unavailable)
+    def to_binary(self, id):
+        # max number of bits needed to encode position
+        max_length = len(bin(self.size[0]*self.size[1] - 1)[2:])
+        bin_repr = []
+        for key, value in self.current_states.items():
+            value = value[0] + self.size[0] * value[1]
+            position = [int(x) for x in bin(value)[2:]]
+            # nb of bits needs to be the same for every position
+            while len(position) < max_length:
+                position.insert(0, 0)
+            # put the agent's position first
+            if key == id:
+                position.extend(bin_repr)
+                bin_repr = position
+            else:
+                bin_repr.extend(position)
+        available_flags = [1 for flag in self.flags]
+        taken_flags = [0 for flag in self.flags]
+        for key, taken in self.flags_taken.items():
+            for flag in taken:
+                flag_idx = self.flags.index(flag)
+                available_flags[flag_idx] = 0
+                if key == id:
+                    taken_flags[flag_idx] = 1
+        bin_repr.extend(available_flags)
+        bin_repr.extend(taken_flags)
+        return bin_repr
 
-	def step(self, id, action):
-		position = self.move(self.current_states[id], action)
-		if self.at_flag(position):
-			if not self.flag_taken(position):
-				self.take_flag(id, position) #take_flag modifies self.flags_taken
-		self.current_states[id] = position
-		reward = self.reward(id, position)
-		terminal = self.terminal(position)
-		return self.to_binary(position), reward, terminal
+    def start(self, id):
+        return self.to_binary(id)
 
+    def step(self, id, action):
+        position = self.move(self.current_states[id], action)
+        if self.at_flag(position):
+            if not self.flag_taken(position):
+                # take_flag modifies self.flags_taken
+                self.take_flag(id, position)
+        self.current_states[id] = position
+        reward = self.reward(id, position)
+        terminal = self.terminal(position)
+        return self.to_binary(id), reward, terminal
--- src/strips.py
+++ src/strips.py
@@ -1,4 +1,4 @@
-from learners.Q import Q
+from learners.Sarsa import Sarsa
 from learners.RewardShaping import RewardShaping
 from shaping.STRIPS import STRIPS
 from policies.EGreedy import EGreedy
@@ -23,8 +23,7 @@ grid = []
 with open("grid.txt") as f:
     for line in f:
         grid.insert(0,literal_eval(line[:-1])) #Opposite order
-grid = np.array(grid)
-
+grid_size = np.shape(grid)
 #monitors
 printer = Printer()
 printer.follow(episode_ended)
@@ -34,25 +33,41 @@ writer.follow(episode_ended)
 writer.activate()
 
 
-nr_of_agents = 2
-environment = RoomWorld(grid, population_size=nr_of_agents, ids=np.arange(nr_of_agents),begins=[[7,4], [7,14]],goal=[1,1])
-
-def state_to_strips(env, id, state):
+flags = []
+for row in grid:
+    for pos in row:
+        if pos[1]:
+            flags.append(pos[1])
+pos_length = len(bin(grid_size[0]*grid_size[1]-1)[2:])
+num_flags = len(flags) 
+def state_to_strips(state):
+    # first bits are agent's position (binary encoded)
+    pos_idx = 0
+    for bit in state[:pos_length]:
+        pos_idx = (pos_idx << 1) | bit
+    pos = [pos_idx % grid_size[0], pos_idx / grid_size[0]]
+    # last bits are agent's flags
+    taken = state[-num_flags:]
     strips_state = set([])
-    strips_state.add('in_' + grid[state][2])
-    for flag in env.flags_taken[id]:
-        strips_state.add('taken_flag' + flag)
+    for i in range(num_flags):
+        if taken[i]:
+            strips_state.add('taken_flag' + flags[i])
+    strips_state.add('in_' + grid[pos[0]][pos[1]][2])
     return strips_state
 
-shaper = STRIPS(strips_plan=strips_plans_individual[0], convert=state_to_strips, omega=600/9)
+nr_of_agents = 2
+learners = []
+#agents
+for i in range(nr_of_agents):
+    shaper = STRIPS(strips_plan=strips_plans_individual[i], convert=state_to_strips, omega=600/9)
+    #agent
+    e_greedy = EGreedy(epsilon=0.2)
+    no_features = Feature(state_length= pos_length*nr_of_agents + len(flags)*2)
+    trace = Eligibility(lambda_=0.9, actions=actions, shape=(no_features.num_features(), len(actions)))
+    sarsa = Sarsa(actions=actions, alpha=0.1, gamma=0.9, policy=e_greedy, features=no_features, trace=trace)
+    learners.append(RewardShaping(learner=sarsa, shaper=shaper))
 
-#agent
-e_greedy = EGreedy(epsilon=0.2)
-grid_size = np.shape(grid)
-no_features = Feature(state_length= grid_size[0]*grid_size[1])
-trace = Eligibility(lambda_=0.9, actions=actions, shape=(no_features.num_features(), len(actions)))
-q = Q(actions=actions, alpha=0.1, gamma=0.9, policy=e_greedy, features=no_features, trace=trace)
-learners = [RewardShaping(learner=q, shaper=shaper) for i in range(nr_of_agents)]
+environment = RoomWorld(grid, population_size=nr_of_agents, ids=[l.id for l in learners], begins=[[7,4], [7,14]],goal=[1,1])
 
 experiment = MultiGeneric(max_steps=None, episodes=5000, trials=1, learners=learners, environment=environment)
 
