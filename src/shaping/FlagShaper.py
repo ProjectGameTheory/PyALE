81f496a8bc43d995f7a0c81bdc7a36bc78d365c9
Mathieu Reymond <mathieu.reymond@gmail.com>
2015-12-28 10:29:44 +0100

    FlagShaper with new state representation

--- src/shaping/FlagShaper.py
+++ src/shaping/FlagShaper.py
@@ -1,19 +1,28 @@
 from Shaper import Shaper
+import numpy as np
 
 class FlagShaper(Shaper):
 
-	def __init__(self, num_flags, **kwargs):
-		super(FlagShaper,self).__init__(**kwargs)
-		self.num_flags = num_flags
-		self.flags_collected = 0
+    def __init__(self, num_flags, grid_length=234, **kwargs):
+        super(FlagShaper,self).__init__(**kwargs)
+        self.num_flags = num_flags
+        self.grid_length = grid_length
+        self.flags_collected = 0
 
-	def start(self):
-		self.flags_collected = 0
+    def start(self):
+        self.flags_collected = 0
 
-	def count_flags(self, state):
-		flags = state[-self.num_flags:]
-		self.flags_collected = sum(flags == 1)
+    def count_flags(self, state):
+        agent_idx = np.where(state == 1)[0][0]
+        flag_idx = agent_idx / self.grid_length
+        # convert flag_idx in flags taken
+        taken = bin(flag_idx)[2:]
+        total = 0
+        for i in range(len(taken)):
+            if taken[i] == '1':
+                total += 1
+        self.flags_collected = total
 
-	def potential(self, state):
-		self.count_flags(state)
-		return 100 * self.flags_collected
+    def potential(self, state):
+        self.count_flags(state)
+        return 100 * self.flags_collected
