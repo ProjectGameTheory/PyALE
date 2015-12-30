9d88db6d40ebbe667a5de0b70f23dd73bd2b59b5
Arno Moonens <x-006@hotmail.com>
2015-12-30 10:07:00 +0100

    Improved plotting

--- src/plots.py
+++ src/plots.py
@@ -18,26 +18,29 @@ for experiment_name, experiment_properties in experiments.iteritems():
     a = df.index.values
     idx = np.array([a] * experiment_properties['nr_of_agents']).T.flatten()[:len(a)]
     df = df.groupby(idx).mean()
-    trial_means = pandas.rolling_mean(df.groupby('episode').reward.mean().values, 5)
-    #trial_medians = pandas.rolling_mean(df.groupby('episode').reward.median().values, 5)
-    trial_sems = pandas.rolling_mean(df.groupby('episode').reward.apply(sem).mul(1.96).values, 5)
+    trial_means = pandas.rolling_mean(df.groupby('episode').reward.mean().values, 20)
+    #trial_medians = pandas.rolling_mean(df.groupby('episode').reward.median().values, 20)
+    trial_sems = pandas.rolling_mean(df.groupby('episode').reward.apply(sem).mul(1.96).values, 20)
     episodes = df.groupby('episode').reward.mean().keys()
+    # Uncomment the lines below to plot error bars
     # pl.fill_between(episodes, trial_means - trial_sems,
     #              trial_means + trial_sems, color="#3F5D7D")
     pl.plot(episodes, trial_means, lw=1,label=experiment_name)
 
-#pl.ylim(-2000,1000)
+
+pl.ylim(-2000,1000)
 pl.xlabel('Episode')
 pl.ylabel('Discounted total reward per episode')
 
 # Shrink current axis's height by 10% on the bottom
 box = ax.get_position()
 ax.set_position([box.x0, box.y0 + box.height * 0.1,
-                 box.width, box.height * 0.9])
+                 box.width, box.height])
 
 # Put a legend below current axis
-ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05),
-          fancybox=True, shadow=True, ncol=5)
+ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1),
+          fancybox=True, shadow=True, ncol=3)
+pl.savefig('rewards.png', bbox_inches='tight')
 pl.show()
 
 
@@ -52,25 +55,26 @@ for experiment_name, experiment_properties in experiments.iteritems():
 
     a = df.index.values
     idx = np.array([a] * experiment_properties['nr_of_agents']).T.flatten()[:len(a)]
-    df = df.groupby(idx).mean()
-    trial_means = pandas.rolling_mean(df.groupby('episode').steps.mean().values, 5)
-    #trial_medians = pandas.rolling_mean(df.groupby('episode').steps.median().values, 5)
-    trial_sems = pandas.rolling_mean(df.groupby('episode').steps.apply(sem).mul(1.96).values, 5)
+    df = df.groupby(idx).sum()
+    trial_means = pandas.rolling_mean(df.groupby('episode').steps.mean().values, 20)
+    #trial_medians = pandas.rolling_mean(df.groupby('episode').steps.median().values, 20)
+    trial_sems = pandas.rolling_mean(df.groupby('episode').steps.apply(sem).mul(1.96).values, 20)
     episodes = df.groupby('episode').steps.mean().keys()
+    # Uncomment the lines below to plot error bars
     # pl.fill_between(episodes, trial_means - trial_sems,
     #              trial_means + trial_sems, color="#3F5D7D")
     pl.plot(episodes, trial_means, lw=1,label=experiment_name)
 
-#pl.ylim(-100,200)
 pl.xlabel('Episode')
 pl.ylabel('Discounted steps per episode')
 
 # Shrink current axis's height by 10% on the bottom
 box = ax.get_position()
 ax.set_position([box.x0, box.y0 + box.height * 0.1,
-                 box.width, box.height * 0.9])
+                 box.width, box.height])
 
 # Put a legend below current axis
-ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.05),
-          fancybox=True, shadow=True, ncol=5)
+ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.1),
+          fancybox=True, shadow=True, ncol=3)
+pl.savefig('steps.png', bbox_inches='tight')
 pl.show()
