1bc5dd329c479eb60c684f1bc4b06fdbf984786d
Arno Moonens <x-006@hotmail.com>
2016-01-03 10:48:09 +0100

    Added strips_plan_5 and associated experiment

--- src/strips_plans.py
+++ src/strips_plans.py
@@ -88,6 +88,40 @@ set(['in_roomD', 'taken_flagF', 'taken_flagE', 'taken_flagC']),
 set(['in_roomD', 'taken_flagF', 'taken_flagE', 'taken_flagC', 'taken_flagD'])]
 ]
 
+strips_plan_5 = [
+#Agent 1 (A, C, E, B, D)
+[set(['in_hallA']),
+set(['in_roomA']),
+set(['in_roomA', 'taken_flagA']),
+set(['in_hallA', 'taken_flagA']),
+set(['in_hallB', 'taken_flagA']),
+set(['in_roomC', 'taken_flagA']),
+set(['in_roomC', 'taken_flagA', 'taken_flagC']),
+set(['in_roomE', 'taken_flagA', 'taken_flagC']),
+set(['in_roomE', 'taken_flagA', 'taken_flagC', 'taken_flagE']),
+set(['in_roomC', 'taken_flagA', 'taken_flagC', 'taken_flagE']),
+set(['in_hallB', 'taken_flagA', 'taken_flagC', 'taken_flagE']),
+set(['in_roomB', 'taken_flagA', 'taken_flagC', 'taken_flagE']),
+set(['in_roomB', 'taken_flagA', 'taken_flagC', 'taken_flagE', 'taken_flagB']),
+set(['in_hallB', 'taken_flagA', 'taken_flagC', 'taken_flagE', 'taken_flagB']),
+set(['in_hallA', 'taken_flagA', 'taken_flagC', 'taken_flagE', 'taken_flagB']),
+set(['in_roomD', 'taken_flagA', 'taken_flagC', 'taken_flagE', 'taken_flagB']),
+set(['in_roomD', 'taken_flagA', 'taken_flagC', 'taken_flagE', 'taken_flagB', 'taken_flagD'])]
+#Agent 2 (F, E, C, B, D)
+[set(['in_roomE']),
+set(['in_roomE', 'taken_flagF']),
+set(['in_roomE', 'taken_flagF', 'taken_flagE']),
+set(['in_roomC', 'taken_flagF', 'taken_flagE']),
+set(['in_roomC', 'taken_flagF', 'taken_flagE', 'taken_flagC']),
+set(['in_hallB', 'taken_flagF', 'taken_flagE', 'taken_flagC']),
+set(['in_roomB', 'taken_flagF', 'taken_flagE', 'taken_flagC']),
+set(['in_roomB', 'taken_flagF', 'taken_flagE', 'taken_flagC', 'taken_flagB']),
+set(['in_hallB', 'taken_flagF', 'taken_flagE', 'taken_flagC', 'taken_flagB']),
+set(['in_hallA', 'taken_flagF', 'taken_flagE', 'taken_flagC', 'taken_flagB']),
+set(['in_roomD', 'taken_flagF', 'taken_flagE', 'taken_flagC', 'taken_flagB']),
+set(['in_roomD', 'taken_flagF', 'taken_flagE', 'taken_flagC', 'taken_flagB', 'taken_flagD'])]
+]
+
 
 
 
