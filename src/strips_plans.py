a89d84f9082c4613b39e63d1bacffcc1957741c0
JensNevens <jens.nevens@gmail.com>
2016-01-03 10:01:42 +0100

    Plan-based-4 and experiment

--- src/strips_plans.py
+++ src/strips_plans.py
@@ -60,6 +60,41 @@ set(['in_roomD', 'taken_flagF', 'taken_flagE', 'taken_flagC', 'taken_flagB', 'ta
 set(['in_roomD', 'taken_flagF', 'taken_flagE', 'taken_flagC', 'taken_flagB', 'taken_flagA', 'taken_flagD'])]
 ]
 
+strips_plan_4 = [
+#Agent 1 (A,B,C,D)
+[set(['in_hallA']),
+set(['in_roomA']),
+set(['in_roomA', 'taken_flagA']),
+set(['in_hallA', 'taken_flagA']),
+set(['in_hallB', 'taken_flagA']),
+set(['in_roomB', 'taken_flagA']),
+set(['in_roomB', 'taken_flagA', 'taken_flagB']),
+set(['in_hallB', 'taken_flagA', 'taken_flagB']),
+set(['in_roomC', 'taken_flagA', 'taken_flagB']),
+set(['in_roomC', 'taken_flagA', 'taken_flagB', 'taken_flagC']),
+set(['in_hallB', 'taken_flagA', 'taken_flagB', 'taken_flagC']),
+set(['in_hallA', 'taken_flagA', 'taken_flagB', 'taken_flagC']),
+set(['in_roomD', 'taken_flagA', 'taken_flagB', 'taken_flagC']),
+set(['in_roomD', 'taken_flagA', 'taken_flagB', 'taken_flagC', 'taken_flagD'])]
+#Agent 2 (F,E,C,D)
+[set(['in_roomE']),
+set(['in_roomE', 'taken_flagF']),
+set(['in_roomE', 'taken_flagF', 'taken_flagE']),
+set(['in_roomC', 'taken_flagF', 'taken_flagE']),
+set(['in_roomC', 'taken_flagF', 'taken_flagE', 'taken_flagC']),
+set(['in_hallB', 'taken_flagF', 'taken_flagE', 'taken_flagC']),
+set(['in_hallA', 'taken_flagF', 'taken_flagE', 'taken_flagC']),
+set(['in_roomD', 'taken_flagF', 'taken_flagE', 'taken_flagC']),
+set(['in_roomD', 'taken_flagF', 'taken_flagE', 'taken_flagC', 'taken_flagD'])]
+]
+
+
+
+
+
+
+
+
 
 
 
